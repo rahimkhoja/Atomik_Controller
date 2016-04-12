0df1fa3d142c5c2b208c824593c20c814d498851
Rahim Khoja <rahimk@khojacorp.com>
2016-04-12 17:06:17 +0000

    atomikCypher.h edited online with Bitbucket

--- atomik_emulator/atomikCypher.h
+++ atomik_emulator/atomikCypher.h
@@ -4,18 +4,16 @@
 class atomikCypher
 {
      private :
+          void init();
           int hour;
           int minute;
           int second;
      public :
           //with default value
-          Time(const int h = 0, const int m  = 0, const int s = 0);
+          atomikCypher();
           //	setter function
-          void setTime(const int h, const int m, const int s);
+          std:string getAtomikJSON(int h, int m, int s);
           // Print a description of object in " hh:mm:ss"
-          void print() const;
-          //compare two time object
-          bool equals(const Time&);
 };
  
 #endif
