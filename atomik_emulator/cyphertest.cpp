616194e7bc3dcf75ad37ca911ab39f4bb8e96cfa
Rahim Khoja <rahimk@khojacorp.com>
2016-04-12 17:26:00 +0000

    cyphertest.cpp created online with Bitbucket

---
+++ atomik_emulator/cyphertest.cpp
@@ -0,0 +1,11 @@
+ 
+#include <iostream>
+using namespace std;
+#include "atomikCypher.h"
+
+int main()
+{
+     atomikCypher cypher();
+     cout << cypher.getAtomikJSON(64, 134, 0) << endl;
+     return 0;
+}
