0c4aa4636eb1f49112bfcf1a32f104784dfe6028
Rahim Khoja <rahimk@khojacorp.com>
2016-04-12 18:07:17 +0000

    atomikCypher.cpp edited online with Bitbucket

--- atomik_emulator/atomikCypher.cpp
+++ atomik_emulator/atomikCypher.cpp
@@ -1,32 +1,6 @@
 
 #include <sstream>
 
-
-namespace Atomik    {
-    typedef std::tuple<int, int, int> key_t;
-    struct key_hash : public std::unary_function<key_t, std::size_t>
-        {
-            std::size_t operator()(const key_t& k) const
-                {
-                    return std::get<0>(k) ^ std::get<1>(k) ^ std::get<2>(k);
-                }
-        };
-
-    struct key_equal : public std::binary_function<key_t, key_t, bool>
-        {
-            bool operator()(const key_t& v0, const key_t& v1) const
-                {
-                    return (
-                               std::get<0>(v0) == std::get<0>(v1) &&
-                               std::get<1>(v0) == std::get<1>(v1) &&
-                               std::get<2>(v0) == std::get<2>(v1) );
-                }
-        };
-
-    typedef std::unordered_map<const key_t,std::string,key_hash,key_equal> map_t;
-
-};
-
 #include "atomikCypher.h"
  
 atomikCypher :: atomikCypher() 
