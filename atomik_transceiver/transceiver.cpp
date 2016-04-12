f384c792c34fdaeab442348bd6d916dc85ee7428
Rahim Khoja <rahimk@khojacorp.com>
2016-04-12 20:53:09 +0000

    transceiver.cpp edited online with Bitbucket

--- atomik_transceiver/transceiver.cpp
+++ atomik_transceiver/transceiver.cpp
@@ -28,118 +28,6 @@ static int debug = 0;
 static int dupesPrinted = 0;
 
 
-void *task1(void *);
-
-static int connFd;
-
-void socketConnection () 
-{
-
-  int pId, portNo, listenFd;
-  socklen_t len; //store size of the address
-  bool loop = false;
-  struct sockaddr_in svrAdd, clntAdd;
-    
-  pthread_t threadA[3];
-    
-//  if (argc < 2)
-//  {
-//      cerr << "Syntam : ./server <port>" << endl;
-//      return 0;
-//  }
-    
-    portNo = atoi(4242);
-    
-    if((portNo > 65535) || (portNo < 2000))
-    {
-        cerr << "Please enter a port number between 2000 - 65535" << endl;
-        return 0;
-    }
-    
-    //create socket
-    listenFd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if(listenFd < 0)
-    {
-        cerr << "Cannot open socket" << endl;
-        return 0;
-    }
-    
-    bzero((char*) &svrAdd, sizeof(svrAdd));
-    
-    svrAdd.sin_family = AF_INET;
-    svrAdd.sin_addr.s_addr = INADDR_ANY;
-    svrAdd.sin_port = htons(portNo);
-    
-    //bind socket
-    if(bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0)
-    {
-        cerr << "Cannot bind" << endl;
-        return 0;
-    }
-    
-    listen(listenFd, 5);
-    
-    len = sizeof(clntAdd);
-    
-    int noThread = 0;
-
-    while (noThread < 3)
-    {
-        cout << "Listening" << endl;
-
-        //this is where client connects. svr will hang in this mode until client conn
-        connFd = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
-
-        if (connFd < 0)
-        {
-            cerr << "Cannot accept connection" << endl;
-            return 0;
-        }
-        else
-        {
-            cout << "Connection successful" << endl;
-        }
-        
-//        pthread_create(&threadA[noThread], NULL, task1, NULL); 
-        
- //       noThread++;
-    }
-    
-   // for(int i = 0; i < 3; i++)
-   // {
-   //     pthread_join(threadA[i], NULL);
-   // }
-    
-
-
-
-}
-
-
-void *task1 (void *dummyPt)
-{
-    cout << "Thread No: " << pthread_self() << endl;
-    char test[300];
-    bzero(test, 301);
-    bool loop = false;
-    while(!loop)
-    {    
-        bzero(test, 301);
-        
-        
-        read(connFd, test, 300);
-        
-        string tester (test);
-        cout << tester << endl;
-        
-        
-        if(tester == "exit")
-            break;
-    }
-    cout << "\nClosing thread and conn" << endl;
-    close(connFd);
-}
 
 void receive()
 {
@@ -275,7 +163,10 @@ int main(int argc, char** argv)
   
   const char *options = "hdfslumn:p:q:r:c:b:k:v:w:";
 
- std::thread socketCommand(socketConnection);
+  std::thread foo([]() { 
+        std::cout << "Hello World" << std::endl;
+    });
+    foo.join();
 
 
   while((c = getopt(argc, argv, options)) != -1){
