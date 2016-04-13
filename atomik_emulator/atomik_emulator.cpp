053b94747e1b601dd818cfc3b1fe2b61b9872468
Rahim Khoja <rahimk@khojacorp.com>
2016-04-13 01:45:30 +0000

    atomik_emulator.cpp edited online with Bitbucket

--- atomik_emulator/atomik_emulator.cpp
+++ atomik_emulator/atomik_emulator.cpp
@@ -60,9 +60,9 @@ std::string getTime(){
     int milli = curTime.tv_usec / 1000;
 
     
-    time(&(time_t)curTime.tv_sec);
+    time((time_t)curTime.tv_sec);
     char buf[sizeof "2011-10-08T07:07:09"];
-    strftime(buf, sizeof buf, "%FT%T", gmtime(&now));
+    strftime(buf, sizeof buf, "%FT%T", gmtime((time_t)curTime.tv_sec));
     // this will work too, if your compiler doesn't support %F or %T:
     //  strftime(buf, sizeof buf, "%Y-%m-%dT%H:%M:%SZ", gmtime(&now));
     sprintf(buf, "%s.%dZ", buf, milli);
