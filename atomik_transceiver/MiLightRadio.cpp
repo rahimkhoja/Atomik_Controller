f4d1e1fdb426eac0bc0413ef88b2129c103a1f95
Rahim Khoja <rahimk@khojacorp.com>
2016-04-24 14:36:08 +0000

    MiLightRadio.cpp edited online with Bitbucket

--- atomik_transceiver/MiLightRadio.cpp
+++ atomik_transceiver/MiLightRadio.cpp
@@ -9,7 +9,8 @@
 
 #define PACKET_ID(packet) ( ((packet[1] & 0xF0)<<24) | (packet[2]<<16) | (packet[3]<<8) | (packet[7]) )
 
-static const uint8_t CHANNELS[] = {9, 40, 71};
+static const uint8_t CHANNELS[] = {4, 39, 74};   // White Channels
+//static const uint8_t CHANNELS[] = {9, 40, 71};  // RGBW Channels
 #define NUM_CHANNELS (sizeof(CHANNELS)/sizeof(CHANNELS[0]))
 
 MiLightRadio::MiLightRadio(AbstractPL1167 &pl1167)
@@ -39,10 +40,16 @@ int MiLightRadio::begin()
     return retval;
   }
 
-  retval = _pl1167.setSyncword(0x147A, 0x258B);
+  //retval = _pl1167.setSyncword(0x147A, 0x258B);    // RGBW Syncword
+  //if (retval < 0) {
+  //  return retval;
+  //}
+
+  retval = _pl1167.setSyncword(0x050A, 0x55AA);      // White Syncword
   if (retval < 0) {
     return retval;
   }
+
 
   retval = _pl1167.setMaxPacketLength(8);
   if (retval < 0) {
