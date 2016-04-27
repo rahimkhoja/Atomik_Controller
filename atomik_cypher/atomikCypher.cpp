1908e294019f6e4634ca31258acb6028ea3b4940
Rahim Khoja <rahimk@khojacorp.com>
2016-04-27 16:46:48 +0000

    atomikCypher.cpp edited online with Bitbucket

--- atomik_cypher/atomikCypher.cpp
+++ atomik_cypher/atomikCypher.cpp
@@ -830,7 +830,7 @@ std::string atomikCypher :: getRadioAtomikJSON(int command, int color, int brigh
 	} else {
 	  cypherValue = 0;
 	}
-	
+	printf("Command: %d, Brightness: %d, Color: %d\n",command, color, brightness);
     auto t = MiLightRadiotoJSONCypher.find(std::make_pair(cypherCommand, cypherValue));
     if (t == MiLightRadiotoJSONCypher.end()) return std::string();
         buffer << t->second;
