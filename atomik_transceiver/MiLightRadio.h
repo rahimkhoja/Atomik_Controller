738d41313b33544d780649de8e91fd6d59b7b5e5
Rahim Khoja <rahimk@khojacorp.com>
2016-04-28 19:18:19 +0000

    MiLightRadio.h edited online with Bitbucket

--- atomik_transceiver/MiLightRadio.h
+++ atomik_transceiver/MiLightRadio.h
@@ -27,7 +27,6 @@ class MiLightRadio {
     int dupesReceived();
     int write(uint8_t frame[], size_t frame_length);
     int resend();
-    void setRadioMode(int mode);
     int setRadioMode(int mode);
   private:
     AbstractPL1167 &_pl1167;
