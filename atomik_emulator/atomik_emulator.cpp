8ea02b5c739f2c7104ceb1f2780fe3109924efc5
Rahim Khoja <rahimk@khojacorp.com>
2016-05-04 22:13:10 +0000

    atomik_emulator.cpp edited online with Bitbucket

--- atomik_emulator/atomik_emulator.cpp
+++ atomik_emulator/atomik_emulator.cpp
@@ -161,7 +161,8 @@ void listen()
         s1 = fread(reply, 1, 15, fd);
         reply[s1] = ',';
         s1++;
-        fd = popen("ifconfig | grep \"HWaddr\" | cut -d ' ' -f 11 | tr -d [:space:] | tr -d ':' | tr [:lower:] [:upper:]", "r");
+        fd = popen("ifconfig | awk '/HWaddr/&&/wlan0/' | cut -d ' ' -f 11 | tr -d [:space:] | tr -d ':' | tr [:lower:] [:upper:]", "r");
+        //fd = popen("ifconfig | grep \"HWaddr\" | cut -d ' ' -f 11 | tr -d [:space:] | tr -d ':' | tr [:lower:] [:upper:]", "r");
         s2 = fread(reply + s1, 1, 12 ,fd);
         reply[s1 + s2] = ',';
     }
