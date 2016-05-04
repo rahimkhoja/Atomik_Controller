8ceb179d8cc1c0ce35325f75136b4d571c7e8459
Rahim Khoja <rahimk@khojacorp.com>
2016-05-04 14:32:09 -0700

    added

--- atomik_web/html/dashboard.php
+++ atomik_web/html/dashboard.php
@@ -156,9 +156,28 @@
       </tr>
       <tr>
         <td>Eth0 MAC Address: </td>
-        <td><?
-exec("/sbin/ifconfig | grep HWaddr", $output);
-print_r( $output);
+        <td><?php
+function getMacLinux() {
+  exec('netstat -ie', $result);
+  if(is_array($result)) {
+    $iface = array();
+    foreach($result as $key => $line) {
+      if($key > 0) {
+        $tmp = str_replace(" ", "", substr($line, 0, 10));
+        if($tmp <> "") {
+          $macpos = strpos($line, "HWaddr");
+          if($macpos !== false) {
+            $iface[] = array('iface' => $tmp, 'mac' => strtolower(substr($line, $macpos+7, 17)));
+          }
+        }
+      }
+    }
+    return $iface[0]['mac'];
+  } else {
+    return "notfound";
+  }
+}
+echo getMacLinux();
 ?></td>
       </tr>
     </tbody>
