b5115a7527e7b1a6f84f2ddd0a2c9df8e6844cc0
Rahim Khoja <rahimk@khojacorp.com>
2016-05-06 00:19:46 -0700

    web pages modified

--- atomik_web/html/dashboard.php
+++ atomik_web/html/dashboard.php
@@ -6,6 +6,31 @@
 <link rel="stylesheet" href="css/atomik.css">
 <?php
 
+$timezone = 'UTC';
+if (is_link('/etc/localtime')) {
+    // Mac OS X (and older Linuxes)    
+    // /etc/localtime is a symlink to the 
+    // timezone in /usr/share/zoneinfo.
+    $filename = readlink('/etc/localtime');
+    if (strpos($filename, '/usr/share/zoneinfo/') === 0) {
+        $timezone = substr($filename, 20);
+    }
+} elseif (file_exists('/etc/timezone')) {
+    // Ubuntu / Debian.
+    $data = file_get_contents('/etc/timezone');
+    if ($data) {
+        $timezone = $data;
+    }
+} elseif (file_exists('/etc/sysconfig/clock')) {
+    // RHEL / CentOS
+    $data = parse_ini_file('/etc/sysconfig/clock');
+    if (!empty($data['ZONE'])) {
+        $timezone = $data['ZONE'];
+    }
+}
+ 
+date_default_timezone_set($timezone);
+
 function getInterfaceMAC($interface) {
   exec('netstat -ie', $result);
   if(is_array($result)) {
@@ -129,6 +154,10 @@ function getCpuUsage() {
   }
 }
 
+function getTimeZone() {
+    return date('T').' - '.date('e').' ( GMT '.date(-e).' ) ';
+  }
+
 ?></head>
 <nav class="navbar navbar-default navbar-inverse">
   <div class="container-fluid"> 
@@ -198,7 +227,7 @@ function getCpuUsage() {
       </tr>
       <tr>
         <td>Current System Time: </td>
-        <td><?php echo date("F j, Y, g:i a"); ?></td>
+        <td><?php echo date(DATE_RFC2822); ?></td>
       </tr>
       <tr>
         <td>System Uptime: </td>
