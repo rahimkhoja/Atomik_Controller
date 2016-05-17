4998eb078e0198564255495e1f5b9fba250925e5
Rahim Khoja <rahimk@khojacorp.com>
2016-05-16 23:01:43 -0700

    Dev

--- atomik_web/html/devices.php
+++ atomik_web/html/devices.php
@@ -1,10 +1,32 @@
-<!doctype html>
+<?php include 'script/database.php';?><!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
 <title>Atomik Controller - Devices</title>
 <link rel="stylesheet" href="css/atomik.css">
-</head>
+<script src="js/jquery-1.12.3.min.js"></script>
+<script src="js/jquery.redirect.min.js"></script>
+</head><?php 
+// Set Default Error & Success Settings
+$page_error = 0;
+$page_success = 0;
+$success_text = "";
+$error = "";
+
+// Atomik Setting SQL
+$sql = "SELECT atomik_devices.device_name, atomik_devices.device_id, atomik_devices.device_status, atomik_device_types.device_type_name FROM atomik_devices, atomik_device_types WHERE atomik_devices.device_type = atomik_device_types.device_type_id;";  
+
+$rs=$conn->query($sql);
+ 
+if($rs === false) {
+  trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+} else {
+  $db_records = $rs->num_rows;
+}
+
+$rs->data_seek(0);
+?>
 <nav class="navbar navbar-default navbar-inverse">
   <div class="container-fluid"> 
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -23,7 +45,7 @@
         <li><a href="tasks.php">Scheduled Tasks</a> </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="logout.php">Logout</a> </li>
+        <li><a id="logoutbtn">Logout</a> </li>
       </ul>
     </div>
     <!-- /.navbar-collapse --> 
@@ -36,17 +58,16 @@
     <div class="row">
         <div class="PageNavTitle" ><h3>Devices</h3></div>
     </div>
-   </div><hr><div class="alert alert-success">
-  <strong>Success!</strong> Indicates a successful or positive action.
-</div><div class="alert alert-danger">
-  <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
-</div>
-<hr>
+   </div><?php if ( $page_success || $page_error ) { ?><hr><?php } ?><?php if ( $page_success ) { ?><div class="alert alert-success">
+  <strong>Success!</strong> <?php echo $success_text; ?>
+</div><?php } ?><?php if ( $page_error ) { ?><div class="alert alert-danger">
+  <strong>Danger!</strong> <?php echo $error_text; ?>
+</div><?php } ?><hr>
 
 <div class="container center-block">
     <div class="col-xs-2"></div>
         <div class="col-xs-4">
-           <h4><p>Device List:</p></h4></div><div class="col-xs-4 text-right"><p><strong>Total Devices: 21</strong></p><p><a href="add_device.php" class="btn-primary btn">Add New Device</a></p>  </div>
+           <h4><p>Device List:</p></h4></div><div class="col-xs-4 text-right"><p><strong>Total Devices: <?php echo $db_records; ?></strong></p><p><a href="add_device.php" class="btn-primary btn">Add New Device</a></p>  </div>
            <div class="col-xs-2"></div>
            </div><br>
            <div class="container center-block">
@@ -61,150 +82,20 @@
         <td></td>
       </tr>
     </thead>
-    <tbody>
+    <tbody>$rs->data_seek(0);
+<?php while($row = $rs->fetch_assoc()){ ?>
     <tr>
-        <td valign="bottom"><center><p>Bedroom 1 Light 1</p></center></td>
+        <td valign="bottom"><center><p><?php echo $row['device_name']; ?></p></center></td>
         <td><center>
-          <p>MiLight RGB WW</p>
+          <p><?php echo $row['device_type_name']; ?></p>
         </center></td>
-        <td><center><p>OFF</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Bedroom 1 Light 2</p></center></td>
-        <td><center>
-          <p>MiLight RGB WW</p>
-        </center></td>
-        <td><center><p>OFF</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Bedroom 1 Night Table Lamp</p></center></td>
-        <td><center>
-          <p>MiLight RGB WW</p>
-        </center></td>
-        <td><center><p>OFF</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Upstairs Hallway Light 1</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Upstairs Hallway Light 2</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Upstairs Bathroom Light 1</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Upstairs Bathroom Light 2</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Bedroom 2 Light 1</p></center></td>
-        <td><center>
-          <p>MiLight RGB WW</p>
-        </center></td>
-        <td><center><p>OFF</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Bedroom 2 Light 2</p></center></td>
-        <td><center>
-          <p>MiLight RGB WW</p>
-        </center></td>
-        <td><center><p>OFF</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center>
-          <p>Downstairs Hallway Light 1</p>
-        </center></td>
-        <td><center>
-          <p>MiLight White</p>
-        </center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center>
-          <p>Downstairs Hallway Light 2</p>
-        </center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Downstairs Bathroom Light 1</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>OFF</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Kitchen Light 1</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Kitchen Light 2</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Kitchen Light 3</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Living Room Light 1</p></center></td>
-        <td><center><p>MiLight RGBW</p></center></td>
-        <td><center><p>OFF</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Living Room Light 2</p></center></td>
-        <td><center><p>MiLight RGBW</p></center></td>
-        <td><center><p>OFF</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Living Room Lamp 1</p></center></td>
-        <td><center><p>MiLight RGBW</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Living Room Lamp 2</p></center></td>
-        <td><center><p>MiLight RGBW</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Porch Light 1</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      <tr>
-        <td><center><p>Porch Light 2</p></center></td>
-        <td><center><p>MiLight White</p></center></td>
-        <td><center><p>ON</p></center></td>
-        <td><center><p><a href="" class="btn-danger btn">Delete Device</a></p></center></td>
-      </tr>
-      
+        <td><center><p><?php if ($row['device_status'] == 1) { echo "ON"; } else { echo "OFF"; }; ?></p></center></td>
+        <td><form id="settingsfrm" name="settingsfrm" action="devices.php" method="post"><input type="hidden" name="item" id="item" value="<?php echo $row['device_id']; ?>" ><center><p><a id="delete<?php echo $row['device_id']; ?>" class="btn-danger btn">Delete Device</a></p></center></form></td>
+        <script type="text/javascript">
+	$("#delete<?php echo $row['device_id']; ?>").on('click', function() {
+   document.delform<?php echo $row['device_id']; ?>.submit();
+});</script>
+      </tr><?php }; ?> 
     </tbody>
   </table>
         
@@ -212,14 +103,14 @@
 </div><div class="container center-block">
     <div class="col-xs-2"></div>
         <div class="col-xs-4">
-           </div><div class="col-xs-4 text-right"><p><strong>Total Devices: 21</strong></p><p><a href="add_device.php" class="btn-primary btn">Add New Device</a></p>  </div>
+           </div><div class="col-xs-4 text-right"><p><strong>Total Devices: <?php echo $db_records; ?></strong></p><p><a href="add_device.php" class="btn-primary btn">Add New Device</a></p>  </div>
            <div class="col-xs-2"></div>
            </div><br>
-  <hr><div class="alert alert-success">
-  <strong>Success!</strong> Indicates a successful or positive action.
-</div><div class="alert alert-danger">
-  <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
-</div><hr>
+  <?php if ( $page_success || $page_error ) { ?><hr><?php } ?><?php if ( $page_success ) { ?><div class="alert alert-success">
+  <strong>Success!</strong> <?php echo $success_text; ?>
+</div><?php } ?><?php if ( $page_error ) { ?><div class="alert alert-danger">
+  <strong>Danger!</strong> <?php echo $error_text; ?>
+</div><?php } ?><hr>
 <div class="push"></div>
  </div>
 <div class="footer FooterColor">
@@ -229,6 +120,10 @@
         <p>Copyright © Atomik Technologies Inc. All rights reserved.</p>
       </div>
       <hr>
-    </div>
+    </div><script type="text/javascript">
+    $("#logoutbtn").on('click', function() {
+	$().redirect('logout.php', {'logout_title': 'Logout', 'description': 'You are now logged out of the Atomik Controller.'});
+});
+</script>
 </body>
 </html>
