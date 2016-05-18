525585efa635ec070972a2192909dfec840bb538
Rahim Khoja <rahimk@khojacorp.com>
2016-05-18 11:43:19 -0700

    Dev

--- atomik_web/html/add_remote.php
+++ atomik_web/html/add_remote.php
@@ -1,17 +1,39 @@
-<!doctype html>
+<?php include 'script/database.php';?><!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
 <title>Atomik Controller - Add Remote</title>
 <link rel="stylesheet" href="css/atomik.css">
+<script src="js/jquery-1.12.3.min.js"></script>
+<script src="js/jquery.redirect.min.js"></script>
+<?php 
+// Set Default Error & Success Settings
+$page_error = 0;
+$page_success = 0;
+$success_text = "";
+$error = "";
+
+// Atomik Setting SQL
+$sql = "SELECT atomik_remote_types.remote_type_name, atomik_remote_types.remote_type_id FROM atomik_remote_types;";  
+
+$rs=$conn->query($sql);
+ 
+if($rs === false) {
+  trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+} else {
+  $db_records = $rs->num_rows;
+}
+$rs->data_seek(0);
+
+?>
 </head>
 <nav class="navbar navbar-default navbar-inverse">
   <div class="container-fluid"> 
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-      <a class="navbar-brand" href="#"><img src="img/Sun_Logo_Menu_50px.gif" width="50" height="50" alt=""/></a></div>
-    
+      <a class="navbar-brand" href="#"><img src="img/Sun_Logo_Menu_50px.gif" width="50" height="50" alt=""/></a></div>    
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
@@ -23,7 +45,7 @@
         <li><a href="tasks.php">Scheduled Tasks</a> </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="logout.php">Logout</a> </li>
+        <li><a id="logoutbtn">Logout</a> </li>
       </ul>
     </div>
     <!-- /.navbar-collapse --> 
@@ -38,62 +60,55 @@
           <h3>Add New Remote</h3>
         </div>
     </div>
-   </div><hr><div class="alert alert-success">
-  <strong>Success!</strong> Indicates a successful or positive action.
-</div><div class="alert alert-danger">
-  <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
-</div>
+   </div><?php if ( $page_success || $page_error ) { ?><hr><?php } ?><?php if ( $page_success ) { ?><div class="alert alert-success">
+  <strong>Success!</strong> <?php echo $success_text; ?>
+</div><?php } ?><?php if ( $page_error ) { ?><div class="alert alert-danger">
+  <strong>Danger!</strong> <?php echo $error_text; ?>
+</div><?php } ?>
 <hr>
   <br>
   <div class="container">
         <div class="col-xs-2"></div>
         <div class="col-xs-8">
-            
-  <table class="table table-striped">
+  <form id="chooseremfrm" name="chooseremfrm" action="device_details.php" method="post"><input name="new" id="new" type="hidden" value="1"><table class="table table-striped">
   <thead>
     <tr>
         <td>
          <h4><p>Please Select Remote Type:</p></h4>
         </td>
-        
     </tr>  
   </thead>
     <tbody>
     <tr>
-        
         <td><p><select id="remotetype" name="remotetype" class="form-control">
-  <option value="3">MiLight RGBW RF Remotes</option>
-  <option value="2">Atomik API Remote</option>
-  <option value="1">MiLight Smartphone Remote</option>
+ <?php while($row = $rs->fetch_assoc()){ ?> <option value="<?php echo $row['remote_type_id']; ?>"><?php echo $row['remote_type_name']; ?></option>
+ <?php }; ?>
 </select></p></td>
-        
       </tr>
       </tbody>
-  </table>
+  </table></form>
 </div>
 <div class="col-xs-2"></div></div>
 <div class="container">
 <div class="col-xs-2"></div>
   <div class="col-xs-4 text-center"></div>
   <div class="col-xs-4 text-center"><p></p></div>
-  
   <div class="col-xs-2"></div>
   </div>
-
-  <br><hr><div class="alert alert-success">
-  <strong>Success!</strong> Indicates a successful or positive action.
-</div><div class="alert alert-danger">
-  <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
-</div><hr>
+  <?php if ( $page_success || $page_error ) { ?><hr><?php } ?><?php if ( $page_success ) { ?><div class="alert alert-success">
+  <strong>Success!</strong> <?php echo $success_text; ?>
+</div><?php } ?><?php if ( $page_error ) { ?><div class="alert alert-danger">
+  <strong>Danger!</strong> <?php echo $error_text; ?>
+</div><?php } ?><hr>
   <div class="container center">
   <div class="col-xs-2">
   </div>
-  <div class="col-xs-1"><a href="remotes.php"  class="btn-warning btn">Cancel</a>
+  <div class="col-xs-1"><a href="remotes.php" class="btn-warning btn">Cancel</a>
   </div>
   <div class="col-xs-1"></div>
   <div class="col-xs-4">
   </div>
-  <div class="col-xs-2 text-right"><a href="" class="btn-success btn">Save & Next</a>
+  <div class="col-xs-2 text-right"><a id="newdevsave" class="btn-success btn">Save & Next</a>
   </div>
   <div class="col-xs-2">
   </div>
@@ -108,6 +123,16 @@
         <p>Copyright © Atomik Technologies Inc. All rights reserved.</p>
       </div>
       <hr>
-    </div>
-</body>
+    </div><script type="text/javascript">
+    $("#logoutbtn").on('click', function() {
+	$().redirect('logout.php', {'logout_title': 'Logout', 'description': 'You are now logged out of the Atomik Controller.'});
+});
+$("#newdevsave").on('click', function() {
+	document.forms["chooseremfrm"].submit();
+});
+</script>
+</body><?php
+$rs->free();
+$conn->close();
+?>
 </html>
