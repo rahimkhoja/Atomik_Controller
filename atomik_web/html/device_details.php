2eca18b5c6df4128ae50d8a0ac69e6565d9f8868
Rahim Khoja <rahimk@khojacorp.com>
2016-05-22 21:24:13 -0700

    Dev

--- atomik_web/html/device_details.php
+++ atomik_web/html/device_details.php
@@ -199,12 +199,12 @@ if ($command <> "" && $command !="" && $command == "save_general")
 	$erro = array();
 	if ($_new_device == 1 )
 	{
-		if (!preg_match('/^[a-z0-9\040\.\'\-]+$/i', $_device_name)) {
+		if (!preg_match("/^[a-zA-Z'. -]+$/", $_device_name)) {
 			array_push($erro, "Device Name Contains Illegal Characters, Please Only Use Letters, Numbers, Spaces, and Apostrophes");
 			$_error_device_name = 1;
 		}
 
-		if (!preg_match('/^[a-z0-9\040\.\'\-]+$/i', $_device_description)) {
+		if (!preg_match("/^[a-zA-Z'. -]+$/", $_device_description)) {
 			array_push($erro, "Device Description Contains Illegal Characters, Please Only Use Letters, Numbers, Spaces, and Apostrophes");
 			$_error_device_description = 1;
 		}
@@ -214,12 +214,12 @@ if ($command <> "" && $command !="" && $command == "save_general")
 		{
 			array_push($erro, "No Changes To Save");
 		} else {
-			if (!preg_match('/^[a-z0-9\040\.\'\-]+$/i', $_device_name)) {
+			if (!preg_match("/^[a-zA-Z'. -]+$/", $_device_name)) {
 				array_push($erro, "Device Name Contains Illegal Characters, Please Only Use Letters, Numbers, Spaces, and Apostrophes");
 				$_error_device_name = 1;
 			}
 		
-			if (!preg_match('/^[a-z0-9\040\.\'\-]+$/i', $_device_description)) {
+			if (!preg_match("/^[a-zA-Z'. -]+$/", $_device_description)) {
 				array_push($erro, "Device Description Contains Illegal Characters, Please Only Use Letters, Numbers, Spaces, and Apostrophes");
 				$_error_device_description = 1;
 			}
