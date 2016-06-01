558c06e63f6b87ca4e3d47166881163779f8e5cd
Rahim Khoja <rahimk@khojacorp.com>
2016-06-01 10:37:56 -0700

    Dev

--- atomik_web/html/add_zone_device.php
+++ atomik_web/html/add_zone_device.php
@@ -49,6 +49,7 @@ if ($command <> "" && $command !="" && $command == "add_device")
 		}
 		$page_success = 1;
 		$success_text = "Zone Device Added To Zone DB!";
+		echo "<BR>";
 		echo '<script type="text/javascript">'."$().redirect('zone_details.php', {'zone_id': ".trim($_zone_id)."});</script>";	
 	} else {
     	$page_error = 1;
