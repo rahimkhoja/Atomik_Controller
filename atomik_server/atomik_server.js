22ecaa7776aabcc0f755b83a3f2fe2936334c177
Rahim Khoja <rahimk@khojacorp.com>
2016-06-19 06:16:06 +0000

    atomik_server.js edited online with Bitbucket

--- atomik_server/atomik_server.js
+++ atomik_server/atomik_server.js
@@ -188,7 +188,7 @@ function checkRFJSON ( address1, address2, channel, req ) {
     var sql = "SELECT atomik_zones.zone_id, atomik_zone_remotes.zone_remote_remote_id FROM atomik_zones, atomik_remotes, atomik_zone_remotes WHERE atomik_zones.zone_id=atomik_zone_remotes.zone_remote_zone_id && atomik_zone_remotes.zone_remote_remote_id=atomik_remotes.remote_id && atomik_remotes.remote_address1="+addint1+" && atomik_remotes.remote_address2="+addint2+" && atomik_zone_remotes.zone_remote_channel_number="+parseInt(channel)+";"; 
     console.log(sql);
     
-    connection.query(sql, function(err, rows, req ) {
+    connection.query(sql, function(err, rows ) {
     if (err) throw err;
  
       if ( rows.length > 0) {
