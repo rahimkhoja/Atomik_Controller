3ef5f12d078c3938ada31c2b1d8bfe6a2322173f
Rahim Khoja <rahimk@khojacorp.com>
2016-06-19 02:16:41 +0000

    atomik_server.js edited online with Bitbucket

--- atomik_server/atomik_server.js
+++ atomik_server/atomik_server.js
@@ -38,46 +38,9 @@ function updateCurrentChannel( channel, remote_id ) {
 
 function log_tra_no_execute(channel, date, rec_data, status, colormode, color, whitetemp, bright, add1, add2) {
 
-  if (typeof channel == 'undefined') {
-    cha = 'NULL';
-  } else {
-    cha = '"'+channel+'"';
-  }
-
-  if (typeof status == 'undefined') {
-    sta = 'NULL';
-  } else {
-    sta = '"'+status+'"';
-  }
-
-  if (typeof colormode == 'undefined') {
-    cm = 'NULL';
-  } else {
-    cm = '"'+colormode+'"';
-  }
-
-  if (typeof color == 'undefined') {
-    col = 'NULL';
-  } else {
-    col = color;
-  }
-
-  if (typeof whitetemp == 'undefined') {
-    wt = 'NULL';
-  } else {
-    wt = whitetemp;
-  }
-
-  if (typeof bright == 'undefined') {
-    bri = 'NULL';
-  } else {
-    bri = bright;
-  }
-
-  var sql = 'INSERT INTO atomik_commands_received (command_received_source_type, command_received_channel_id, command_received_date, command_received_data, command_received_status, command_received_color_mode, command_received_rgb256, command_received_white_temprature, command_received_brightness, command_received_processed, command_received_ADD1, command_received_ADD2) VALUES ("Radio", '+cha+', "'+date+'", "'+rec_data+'", '+sta+', '+cm+', '+col+', '+wt+', '+bri+', 0, "'+add1+'", "'+add2+'")';
+  var sql = 'INSERT INTO atomik_commands_received (command_received_source_type, command_received_channel_id, command_received_date, command_received_data, command_received_status, command_received_color_mode, command_received_rgb256, command_received_white_temprature, command_received_brightness, command_received_processed, command_received_ADD1, command_received_ADD2) VALUES ("Radio", '+channel+', "'+date+'", "'+rec_data+'", '+status+', '+colormode+', '+color+', '+whitetemp+', '+bright+', 0, "'+add1+'", "'+add2+'")';
 
   console.log(sql);
-
   connection.query(sql, function(err, rows, fields) {
     if (!err) {
       console.log('The solution is: ', rows);
@@ -94,43 +57,7 @@ function log_tra_no_execute(channel, date, rec_data, status, colormode, color, w
 
 function log_tra_execute(channel, date, rec_data, status, colormode, color, whitetemp, bright, add1, add2) {
 
-  if (typeof channel == 'undefined') {
-    cha = 'NULL';
-  } else {
-    cha = '"'+channel+'"';
-  }
-
-  if (typeof status == 'undefined') {
-    sta = 'NULL';
-  } else {
-    sta = '"'+status+'"';
-  }
-
-  if (typeof colormode == 'undefined') {
-    cm = 'NULL';
-  } else {
-    cm = '"'+colormode+'"';
-  }
-
-  if (typeof color == 'undefined') {
-    col = 'NULL';
-  } else {
-    col = color;
-  }
-
-  if (typeof whitetemp == 'undefined') {
-    wt = 'NULL';
-  } else {
-    wt = whitetemp;
-  }
-
-  if (typeof bright == 'undefined') {
-    bri = 'NULL';
-  } else {
-    bri = bright;
-  }
-
-  var sql = 'INSERT INTO atomik_commands_received (command_received_source_type, command_received_channel_id, command_received_date, command_received_data, command_received_status, command_received_color_mode, command_received_rgb256, command_received_white_temprature, command_received_brightness, command_received_processed, command_received_ADD1, command_received_ADD2) VALUES ("Radio", '+cha+', "'+date+'", "'+rec_data+'", '+sta+', '+cm+', '+col+', '+wt+', '+bri+', 1, "'+add1+'", "'+add2+'")';
+  var sql = 'INSERT INTO atomik_commands_received (command_received_source_type, command_received_channel_id, command_received_date, command_received_data, command_received_status, command_received_color_mode, command_received_rgb256, command_received_white_temprature, command_received_brightness, command_received_processed, command_received_ADD1, command_received_ADD2) VALUES ("Radio", '+channel+', "'+date+'", "'+rec_data+'", '+status+', '+colormode+', '+color+', '+whitetemp+', '+bright+', 1, "'+add1+'", "'+add2+'")';
 
   console.log(sql);
 
@@ -150,12 +77,83 @@ function log_tra_execute(channel, date, rec_data, status, colormode, color, whit
 function validRF(zoneID, req){
   console.log('Valid Command!:');
   console.log("Zone_ID: " + zoneID );
-  log_tra_execute(req.body.Channel, req.body.DateTime, req.body.Data, req.body.Configuration.Status, req.body.Configuration.ColorMode, req.body.Configuration.Color, req.body.Configuration.WhiteMode, req.body.Configuration.Brightness, req.body.Address1, req.body.Address2);
+  if (typeof req.body.Channel == 'undefined') {
+    cha = 'NULL';
+  } else {
+    cha = '"'+req.body.Channel+'"';
+  }
+
+  if (typeof req.body.Configuration.Status == 'undefined') {
+    sta = 'NULL';
+  } else {
+    sta = '"'+req.body.Configuration.Status+'"';
+  }
+
+  if (typeof req.body.Configuration.ColorMode == 'undefined') {
+    cm = 'NULL';
+  } else {
+    cm = '"'+req.body.Configuration.ColorMode+'"';
+  }
+
+  if (typeof req.body.Configuration.Color == 'undefined') {
+    col = 'NULL';
+  } else {
+    col = req.body.Configuration.Color;
+  }
+
+  if (typeof req.body.Configuration.WhiteTemp == 'undefined') {
+    wt = 'NULL';
+  } else {
+    wt = req.body.Configuration.WhiteTemp;
+  }
+
+  if (typeof req.body.Configuration.Brightness == 'undefined') {
+    bri = 'NULL';
+  } else {
+    bri = req.body.Configuration.Brightness;
+  }
+  log_tra_execute(cha, req.body.DateTime, req.body.Data, sta, cm, col, wt, bri, req.body.Address1, req.body.Address2);
 }
 
 function invalidRF(req){
   console.log('Invalid Command:');
-  log_tra_no_execute(req.body.Channel, req.body.DateTime, req.body.Data, req.body.Configuration.Status, req.body.Configuration.ColorMode, req.body.Configuration.Color, req.body.Configuration.WhiteMode, req.body.Configuration.Brightness, req.body.Address1, req.body.Address2);
+  if (typeof req.body.Channel == 'undefined') {
+    cha = 'NULL';
+  } else {
+    cha = '"'+req.body.Channel+'"';
+  }
+
+  if (typeof req.body.Configuration.Status == 'undefined') {
+    sta = 'NULL';
+  } else {
+    sta = '"'+req.body.Configuration.Status+'"';
+  }
+
+  if (typeof req.body.Configuration.ColorMode == 'undefined') {
+    cm = 'NULL';
+  } else {
+    cm = '"'+req.body.Configuration.ColorMode+'"';
+  }
+
+  if (typeof req.body.Configuration.Color == 'undefined') {
+    col = 'NULL';
+  } else {
+    col = req.body.Configuration.Color;
+  }
+
+  if (typeof req.body.Configuration.WhiteTemp == 'undefined') {
+    wt = 'NULL';
+  } else {
+    wt = req.body.Configuration.WhiteTemp;
+  }
+
+  if (typeof req.body.Configuration.Brightness == 'undefined') {
+    bri = 'NULL';
+  } else {
+    bri = req.body.Configuration.Brightness;
+  }
+  
+  log_tra_no_execute(cha, req.body.DateTime, req.body.Data, sta, cm, col, wt, bri, req.body.Address1, req.body.Address2);
 }
 
 function checkRFJSON ( address1, address2, channel, req ) {
@@ -182,7 +180,7 @@ function checkRFJSON ( address1, address2, channel, req ) {
     });
     
   } else {
-    var sql = "SELECT atomik_zones.zone_id, atomik_zone_remotes.zone_remote_remote_id FROM atomik_zones, atomik_remotes, atomik_zone_remotes WHERE atomik_zones.zone_id=atomik_zone_remotes.zone_remote_zone_id && atomik_zone_remotes.zone_remote_remote_id=atomik_remotes.remote_id && atomik_remotes.remote_address1="+addint1+" && atomik_remotes.remote_address2="+addint2+" atomik_zone_remotes.zone_remote_channel_number="+channel+";"; 
+    var sql = "SELECT atomik_zones.zone_id, atomik_zone_remotes.zone_remote_remote_id FROM atomik_zones, atomik_remotes, atomik_zone_remotes WHERE atomik_zones.zone_id=atomik_zone_remotes.zone_remote_zone_id && atomik_zone_remotes.zone_remote_remote_id=atomik_remotes.remote_id && atomik_remotes.remote_address1="+addint1+" && atomik_remotes.remote_address2="+addint2+" atomik_zone_remotes.zone_remote_channel_number="+parseInt(channel)+";"; 
     connection.query(sql, function(err, rows, req ) {
     if (err) throw err;
  
@@ -202,55 +200,14 @@ function checkRFJSON ( address1, address2, channel, req ) {
 
 function log_emu_no_execute(channel, date, rec_data, status, colormode, color, whitetemp, bright, ip, mac) {
 
-if (typeof channel == 'undefined') {
-  cha = 'NULL';
-} else {
-  cha = '"'+channel+'"';
-}
-
-if (typeof status == 'undefined') {
-  sta = 'NULL'; 
-} else {
-  sta = '"'+status+'"';
-}
-
-if (typeof colormode == 'undefined') {
-  cm = 'NULL';
-} else {
-  cm = '"'+colormode+'"';
-}
-
-if (typeof color == 'undefined') {
-  col = 'NULL';
-} else {
-  col = color;
-}
-
-if (typeof whitetemp == 'undefined') {
-  wt = 'NULL';
-} else {
-  wt = whitetemp;
-}
-
-if (typeof bright == 'undefined') {
-  bri = 'NULL';
-} else {
-  bri = bright;
-}
-
-        var sql = 'INSERT INTO atomik_commands_received (command_received_source_type, command_received_channel_id, command_received_date, command_received_data, command_received_status, command_received_color_mode, command_received_rgb256, command_received_white_temprature, command_received_brightness, command_received_processed, command_received_MAC, command_received_IP) VALUES ("Emulator", '+cha+', "'+date+'", "'+rec_data+'", '+sta+', '+cm+', '+col+', '+wt+', '+bri+', 0, "'+mac+'", "'+ip+'")'; 
-
-
-	console.log(sql);
-
-
-	connection.query(sql, function(err, rows, fields) {
- if (!err)
-   console.log('The solution is: ', rows);
- else
-   console.log('Error while performing Query.');
- });
-
+  var sql = 'INSERT INTO atomik_commands_received (command_received_source_type, command_received_channel_id, command_received_date, command_received_data, command_received_status, command_received_color_mode, command_received_rgb256, command_received_white_temprature, command_received_brightness, command_received_processed, command_received_MAC, command_received_IP) VALUES ("Emulator", '+cha+', "'+date+'", "'+rec_data+'", '+sta+', '+cm+', '+col+', '+wt+', '+bri+', 0, "'+mac+'", "'+ip+'")'; 
+  console.log(sql);
+  connection.query(sql, function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+  });
 } 
 
 app.use(bodyParser.json());
