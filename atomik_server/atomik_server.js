4e3cdcc647d00aa526e0b0dfbf81a624addb164c
Rahim Khoja <rahimk@khojacorp.com>
2016-06-20 18:27:17 +0000

    atomik_server.js edited online with Bitbucket

--- atomik_server/atomik_server.js
+++ atomik_server/atomik_server.js
@@ -567,7 +567,7 @@ function transmit(new_b, old_b, new_s, old_s, new_c, old_c, new_wt, old_wt, new_
     // Close White CCT Bulb Transmissiom
   } else if (cw == 1 && rgb == 1 || ww == 1 && rgb == 1) {
     // Start RGB WW and RGB CW Bulb Transmission
-    atomikTransmitCMD(1, add1, add2, old_c, old_b, trans, 3);
+    // atomikTransmitCMD(1, add1, add2, old_c, old_b, trans, 3);
     // RGBWW and RGBCW
     if (new_s != old_s) {
       // Status Changed
