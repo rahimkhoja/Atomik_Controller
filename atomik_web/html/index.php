e29b3fcca8eabb4f6e4cb2129c300a52063f36dc
Rahim Khoja <rahimk@khojacorp.com>
2016-06-21 16:09:20 -0700

    Dev

--- atomik_web/html/index.php
+++ atomik_web/html/index.php
@@ -4,54 +4,30 @@
 <meta charset="utf-8">
 <title>Atomik Controller - Login</title>
 <link rel="stylesheet" href="css/atomik.css">
-<script src="js/jquery-1.12.3.min.js"></script>
-<?php
-	session_start();
-	$db_records = 0; 
-	
-	if ( $_SESSION['is_error'] == 1 ) {
-		$page_error = 1;
-		$error_text = "Invalid Username Or Password!";	
-		$_SESSION['is_error'] == 0;
-	}
-	if (isset($_POST["username"]) && isset($_POST["password"])) {
-	  	$username = $_POST["username"];
-  		$password_sha1 = sha1($_POST["password"]);
-		session_destroy();
-    	session_start();
-	  	if ( $username == "admin" ) {
-  			$sql  = "SELECT atomik_settings.id, atomik_settings.password FROM atomik_settings WHERE atomik_settings.password='".$password_sha1."';";
-			echo $sql;
-  			$rs=$conn->query($sql);
-			if($rs === false) {
-  				trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-			} else {
-  				$db_records = $rs->num_rows;
-			}
-
-		    if ($db_records > 0) {
-		    	$_SESSION['signed_in'] = true;
-        		$_SESSION['username'] = $username;
-       			header("Location: /dashboard.php");
-			} else {
-    			$_SESSION['is_error'] = 1;
-    			$_SESSION['signed_in'] = false;
-    			$_SESSION['username'] = null;
-    			header("Location: /index.php");
-			}
-		}
-	}
-?></head>
+<script src="js/jquery-1.12.3.min.js"></script><?php
+require_once 'script/config.php';
+ 
+// Is the user already logged in? Redirect him/her to the private page
+ 
+if($_SESSION['username'])
+{
+header("Location: dashboard.php");
+exit;
+}
+ 
+if(isSet($_POST['submit']))
+{
+$do_login = true;
+include_once 'script/do_login.php';
+}?></head>
 <nav class="navbar navbar-default navbar-inverse">
   <div class="container-fluid"> 
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
      </div>
-    
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      
     </div>
     <!-- /.navbar-collapse --> 
   </div>
@@ -67,9 +43,8 @@
     </div>
 </div><?php if ( $page_success || $page_error ) { ?><hr><?php } ?><?php if ( $page_success ) { ?><div class="alert alert-success">
   <strong>Success!</strong> <?php echo $success_text; ?>
-</div><?php } ?><?php if ( $page_error ) { ?><div class="alert alert-danger">
-  <strong>Danger!</strong> <?php echo $error_text; ?>
-</div><?php } ?><hr>
+</div><?php } ?><?php if ( $login_error ) { ?><div class="alert alert-danger">
+  <strong>Danger!</strong> Invalid Username Or Password!</div><?php } ?><hr>
   <br>
  <div class="container">
     <div class="row">
@@ -90,12 +65,11 @@
       </tr>
       <tr>
         <td>Save Password: </td>
-        <td><input type="checkbox" class="form-control" id="savepass" name="savepass"></td>
+        <td><input type="checkbox" class="form-control" id="autologin" name="autologin" value="1" checked></td>
       </tr>
     </tbody>
   </table></form></div>
-    </div>
-        </div>
+    </div></div>
   <hr>
   <div class="center col-xs-12">
   <div class="text-center col-xs-12"><p><a id="loginbtn" class="btn-success btn">Login</a></p></div>
@@ -103,13 +77,18 @@
 </div>
  </div><?php if ( $page_success || $page_error ) { ?><hr><?php } ?><?php if ( $page_success ) { ?><div class="alert alert-success">
   <strong>Success!</strong> <?php echo $success_text; ?>
-</div><?php } ?><?php if ( $page_error ) { ?><div class="alert alert-danger">
-  <strong>Danger!</strong> <?php echo $error_text; ?>
+</div><?php } ?><?php if ( $login_error ) { ?><div class="alert alert-danger">
+  <strong>Danger!</strong> Invalid Username Or Password!
 </div><?php } ?><hr>
+<div class="push"></div>
+ </div>
 <div class="footer FooterColor">
-      <div class="col-xs-12 text-center"><BR><BR>
+     <hr>
+      <div class="col-xs-12 text-center">
         <p>Copyright © Atomik Technologies Inc. All rights reserved.</p>
-      </div></div><script type="text/javascript">
+      </div>
+      <hr>
+    </div><script type="text/javascript">
 	$("#loginbtn").on('click', function() {
    document.forms["loginfrm"].submit();
 });</script></body><?php
