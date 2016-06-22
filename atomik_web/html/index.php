957d61fac9ddb1393b81ec6ccd5b1c52ba3b519d
Rahim Khoja <rahimk@khojacorp.com>
2016-06-22 00:02:28 -0700

    Dev

--- atomik_web/html/index.php
+++ atomik_web/html/index.php
@@ -48,7 +48,7 @@ include_once 'script/do_login.php';
  <div class="container">
     <div class="row">
         <div class="col-xs-12"></div>
-        <div class="col-xs-5 text-center" style="overflow:hidden"><img src="img/Sun_Logo_Center_450px.gif" width="300" height="3o0" alt=""/></div>
+        <div class="col-xs-5 text-center" style="overflow:hidden"><img src="img/Sun_Logo_Center_450px.gif" width="300" height="300" alt=""/></div>
         <div class="col-xs-1" style="border-left:1px solid #000;height:350px"></div>
         <div class="col-xs-6 glyphicon-option-vertical"><h4><p>Login:</p></h4><BR><form id="loginfrm" name="loginfrm" action="/index.php" method="post"><table class="table table-striped">
     <thead>
