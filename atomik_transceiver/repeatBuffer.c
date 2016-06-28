f36a8b7688ba1d4383a353ab5b7954c687e5fdd4
Rahim Khoja <rahimk@khojacorp.com>
2016-06-28 01:01:16 +0000

    repeatBuffer.c edited online with Bitbucket

--- atomik_transceiver/repeatBuffer.c
+++ atomik_transceiver/repeatBuffer.c
@@ -44,9 +44,9 @@ void repeatBuffer::removeOldTransmissions() {
 
     for(std::vector<transmission>::iterator it = trans.begin(); it != trans.end(); ++it) {
         double elapsedTime = currentTime - (*it).timestamp;
-        std::cout << (*it) << std:endl;
+        std::cout << (*it) << std::endl;
         if ( elapsedTime > 350000 ) {
-            std::cout << "deleted: " << (*it) << std:endl;
+            std::cout << "deleted: " << (*it) << std::endl;
             trans.erase(it);
         }
     }
