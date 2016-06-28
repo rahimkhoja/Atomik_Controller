4d6ae9305321b54c917feeda5fb16c015b1e2c00
Rahim Khoja <rahimk@khojacorp.com>
2016-06-28 17:31:26 +0000

    repeatBuffer.c edited online with Bitbucket

--- atomik_transceiver/repeatBuffer.c
+++ atomik_transceiver/repeatBuffer.c
@@ -5,103 +5,87 @@
 #include <algorithm>
 #include <sys/time.h>
 
-struct transmissionData
-{
+struct transmissionData {
   int add1;
-  int add2;
+  int add2;  
   int command;
   int color;
   int bright;
   int prefix;
 };
 
-struct transmission
-{
+struct transmission{
   struct transmissionData data;
   double timestamp;
 };
 
-class repeatBuffer
-{
+class repeatBuffer {
 
 private:
+ 
+  std::vector<transmission> trans;
 
-  std::vector < transmission > trans;
 
 
+bool compare_trans( const transmissionData & e1, const transmissionData & e2) {
+  if( e1.add1 == e2.add1 &&  e1.add2 == e2.add2 && e1.color == e2.color && e1.bright == e2.bright && e1.prefix == e2.prefix)
+    return true;
+  return false;
+};
 
-  bool compare_trans (const transmissionData & e1,
-		      const transmissionData & e2)
-  {
-    if (e1.add1 == e2.add1 && e1.add2 == e2.add2 && e1.color == e2.color
-	&& e1.bright == e2.bright && e1.prefix == e2.prefix)
-      return true;
-    return false;
-  };
-
-  void removeOldTransmissions ()
-  {
-
+void removeOldTransmissions() {
+    
     timeval tv;
     gettimeofday (&tv, NULL);
     double currentTime = (tv.tv_sec) + 0.0000001 * tv.tv_usec;
-
-    auto it = trans.begin ();
-    while (it != trans.end ())
-      {
-	double elapsedTime = currentTime - (*it).timestamp;
-	if (elapsedTime > 0.950000)
-	  {
-	    std::cout << std::fixed << "deleted: " << (*it).
-	      timestamp << std::endl;
-	    trans.erase (it);
-	  }
-	else
-	  {
-	    it++;
-	  }
-      }
-    std::cout << "Finished Delete Loop" << std::endl;
-  };
-
-public:
-
-  bool addTransmission (int add1, int add2, int col, int bri, int pf,
-			int command)
-  {
-    std::cout << "Adding Tramsission to Buffer!" << std::endl;
-    bool returnVal = false;
-    transmissionData newTrans;
-    newTrans.add1 = add1;
-    newTrans.add2 = add2;
-    newTrans.bright = bri;
-    newTrans.color = col;
-    newTrans.prefix = pf;
-    newTrans.command = command;
-    removeOldTransmissions ();
-    auto it = trans.begin ();
-    std::cout << "Check for matching transmission" << std::endl;
-    while (it != trans.end ())
-    {
-	  if (compare_trans (newTrans, (*it).data))
-	  {
-	    returnVal = true;
-	  }
     
-      it++;
-    }
-    std::cout << "check loop complete!" << std::endl;
-    transmission newTra;
-    newTra.data = newTrans;
-    timeval tv;
-    gettimeofday (&tv, NULL);
-    newTra.timestamp = (tv.tv_sec) + 0.0000001 * tv.tv_usec;
-    std::cout << newTra.timestamp << std::endl;
-    if (returnVal = false)
-      {
-	trans.push_back (newTra);
+    auto it = trans.begin();
+    while (it != trans.end()) {
+      double elapsedTime = currentTime - (*it).timestamp;
+      std::cout << std::fixed << (*it).timestamp << std::endl;
+      std::cout << std::fixed << currentTime << std::endl;
+      std::cout << std::fixed << elapsedTime << std::endl;
+      if ( elapsedTime > 0.350000 ) { 
+        std::cout << std::fixed << "deleted: " << (*it).timestamp << std::endl;  
+        trans.erase(it);
+      } else {
+        it++;
       }
-    return returnVal;
-
-  };
+    }
+    std::cout << "Finished Delete Loop" << std::endl;  
+} ;
+        
+public:
+  
+bool addTransmission(int add1, int add2, int col, int bri, int pf, int command) {
+  std::cout << "Adding Tramsission to Buffer!" << std::endl;
+  bool returnVal = false;
+  transmissionData newTrans;
+  newTrans.add1 = add1;
+  newTrans.add2 = add2;
+  newTrans.bright = bri;
+  newTrans.color = col;
+  newTrans.prefix = pf;
+  newTrans.command = command;
+  removeOldTransmissions();
+  auto it = trans.begin();
+  std::cout << "Check fro trans" << std::endl;
+  while (it != trans.end()) {
+    if (compare_trans(newTrans, (*it).data)) {
+      returnVal = true;
+      trans.erase(it);
+    } 
+    it++;
+  }
+  std::cout << "check loop compete!" << std::endl;
+  transmission newTra;
+  newTra.data = newTrans;
+  timeval tv;
+  gettimeofday (&tv, NULL);
+  newTra.timestamp = (tv.tv_sec) + 0.0000001 * tv.tv_usec;
+  std::cout << newTra.timestamp << std::endl;
+  trans.push_back(newTra);
+  return returnVal;
+  
+};
 };
