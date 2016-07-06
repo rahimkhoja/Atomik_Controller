d9239a8faa0d31c1efad423e65b6dfc0c3553e8b
Rahim Khoja <rahimk@khojacorp.com>
2016-07-06 17:33:26 +0000

    updateWLAN.sh edited online with Bitbucket

--- atomik_scripts/updateWLAN.sh
+++ atomik_scripts/updateWLAN.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wlan0_ssid=$(mysql -uroot -praspberry -se "SELECT wlan0_ssid FROM atomik_controller.atomik_settings");
-wlan0_method=$(mysql -uroot -praspberry -se "SELECT wlan0_method FROM atomik_controller.atomik_settings"); # 0 = Disable, 1 = Open WEP, 2 = Shared Wep, 3 = WPAPSK, 4 = WPA2PSK
-wlan0_algorithm=$(mysql -uroot -praspberry -se "SELECT wlan0_algorithm FROM atomik_controller.atomik_settings"); # 0 = ASCII, 1 = HEX, 2 = TKIP, 3 = AES
+wlan0_method=($(mysql -uroot -praspberry -se "SELECT wlan0_method FROM atomik_controller.atomik_settings")); # 0 = Disable, 1 = Open WEP, 2 = Shared Wep, 3 = WPAPSK, 4 = WPA2PSK
+wlan0_algorithm=($(mysql -uroot -praspberry -se "SELECT wlan0_algorithm FROM atomik_controller.atomik_settings")); # 0 = ASCII, 1 = HEX, 2 = TKIP, 3 = AES
 wlan0_password=$(mysql -uroot -praspberry -se "SELECT wlan0_password FROM atomik_controller.atomik_settings");
 
 wpa_sup_file=$(sudo cat /etc/wpa_supplicant/wpa_supplicant.conf | sed -e '/network={/,/}/c\network={\n')
