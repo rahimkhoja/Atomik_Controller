647f9dcabf1971d399aa556cd971cc7892853bee
Rahim Khoja <rahimk@khojacorp.com>
2016-07-06 18:31:22 +0000

    updateWLAN.sh edited online with Bitbucket

--- atomik_scripts/updateWLAN.sh
+++ atomik_scripts/updateWLAN.sh
@@ -7,72 +7,72 @@ wlan0_password=$(mysql -uroot -praspberry -se "SELECT wlan0_password FROM atomik
 
 wpa_sup_file=$(sudo cat /etc/wpa_supplicant/wpa_supplicant.conf | sed -e '/network={/,/}/c\network={\n')
 
-sudo echo -e "$wpa_sup_file"
+sudo echo -e "$wpa_sup_file" > /tmp/testwlan.out
 if [ $wlan0_method == "0" ] 
 then 
-printf "\tssid=\"%s\"\n" "$wlan0_ssid"
-echo -e "\tkey_mgmt=NONE"
+printf "\tssid=\"%s\"\n" "$wlan0_ssid" >> /tmp/testwlan.out
+echo -e "\tkey_mgmt=NONE" >> /tmp/testwlan.out
 fi
 
 if [ $wlan0_method == "1" ]
 then
-printf "\tssid=\"%s\"\n" "$wlan0_ssid"
-echo -e "\tkey_mgmt=NONE"
+printf "\tssid=\"%s\"\n" "$wlan0_ssid" >> /tmp/testwlan.out
+echo -e "\tkey_mgmt=NONE" >> /tmp/testwlan.out
 if [ $wlan0_algorithm == "0" ]
 then
-printf "\twep_key0=\"%s\"\n" "$wlan0_password"
+printf "\twep_key0=\"%s\"\n" "$wlan0_password" >> /tmp/testwlan.out
 fi
 if [ $wlan0_algorithm == "1" ]
 then
-printf "\twep_key0=%s\n" "$wlan0_password"
+printf "\twep_key0=%s\n" "$wlan0_password" >> /tmp/testwlan.out
 fi
 
-echo -e "\twep_tx_keyidx=0"
+echo -e "\twep_tx_keyidx=0" >> /tmp/testwlan.out
 fi
 
 if [ $wlan0_method == "2" ]
 then
-printf "\tssid=\"%s\"\n" "$wlan0_ssid"
-echo -e "\tkey_mgmt=NONE"
+printf "\tssid=\"%s\"\n" "$wlan0_ssid" >> /tmp/testwlan.out
+echo -e "\tkey_mgmt=NONE" >> /tmp/testwlan.out
 if [ $wlan0_algorithm == "0" ]
 then
-printf "\twep_key0=\"%s\"\n" "$wlan0_password"
+printf "\twep_key0=\"%s\"\n" "$wlan0_password" >> /tmp/testwlan.out
 fi
 if [ $wlan0_algorithm == "1" ]
 then
-printf "\twep_key0=%s\n" "$wlan0_password"
+printf "\twep_key0=%s\n" "$wlan0_password" >> /tmp/testwlan.out
 fi
 
-echo -e "\twep_tx_keyidx=0"
-echo -e "\tauth_alg=SHARED"
+echo -e "\twep_tx_keyidx=0" >> /tmp/testwlan.out
+echo -e "\tauth_alg=SHARED" >> /tmp/testwlan.out
 fi
 
 if [ $wlan0_method == "3" ]
 then
-printf "\tssid=\"%s\"\n" "$wlan0_ssid"
-echo -e "\tproto=WPA"
-echo -e "\tkey_mgmt=WPA-PSK"
-echo -e "\tpairwise=CCMP TKIP"
-echo -e "\tgroup=CCMP TKIP WEP104 WEP40"
-printf "\tpsk=\"%s\"\n" "$wlan0_password"
+printf "\tssid=\"%s\"\n" "$wlan0_ssid" >> /tmp/testwlan.out
+echo -e "\tproto=WPA" >> /tmp/testwlan.out
+echo -e "\tkey_mgmt=WPA-PSK" >> /tmp/testwlan.out
+echo -e "\tpairwise=CCMP TKIP" >> /tmp/testwlan.out
+echo -e "\tgroup=CCMP TKIP WEP104 WEP40" >> /tmp/testwlan.out
+printf "\tpsk=\"%s\"\n" "$wlan0_password" >> /tmp/testwlan.out
 fi
 
 if [ $wlan0_method == "4" ]
 then
-printf "\tssid=\"%s\"\n" "$wlan0_ssid"
-printf "\tpsk=\"%s\"\n" "$wlan0_password"
-echo -e "\tproto=RSN"
-echo -e "\tkey_mgmt=WPA-PSK"
+printf "\tssid=\"%s\"\n" "$wlan0_ssid" >> /tmp/testwlan.out
+printf "\tpsk=\"%s\"\n" "$wlan0_password" >> /tmp/testwlan.out
+echo -e "\tproto=RSN" >> /tmp/testwlan.out
+echo -e "\tkey_mgmt=WPA-PSK" >> /tmp/testwlan.out
 if [ $wlan0_algorithm == "2" ]
 then
-echo -e "\tpairwise=TKIP"
+echo -e "\tpairwise=TKIP" >> /tmp/testwlan.out
 fi
 if [ $wlan0_algorithm == "3" ]
 then
-echo -e "\tpairwise=CCMP"
+echo -e "\tpairwise=CCMP" >> /tmp/testwlan.out
 fi
 
 fi
 
-sudo echo -e "}"
+sudo echo -e "}"  >> /tmp/testwlan.out
 exit 0
