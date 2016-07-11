38a8424452bcab8123ac082b537bed27a98808d7
Rahim Khoja <rahimk@khojacorp.com>
2016-07-11 21:47:26 +0000

    updateSSIDlist.sh edited online with Bitbucket

--- atomik_scripts/updateSSIDlist.sh
+++ atomik_scripts/updateSSIDlist.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Atomik Controller Script - Update Available WiFi's
+# Bash
+# By Rahim Khoja
+
+# Requires Root
+
 sudo iwlist wlan0 scan | grep SSID: | cut -d: -f2 | tr '\"' ' ' | awk {'print $1'} > /var/atomik/www/ssid.txt
 sudo chown www-data:www-data /var/atomik/www/ssid.txt
