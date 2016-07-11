45c5ba17b85f4d242886b203a38f026a0799326a
Rahim Khoja <rahimk@khojacorp.com>
2016-07-11 21:48:37 +0000

    updateHOSTNAME.sh edited online with Bitbucket

--- atomik_scripts/updateHOSTNAME.sh
+++ atomik_scripts/updateHOSTNAME.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Atomik Controller Script - Update System Hostname
+# Bash
+# By Rahim Khoja
+
+# Requires Root, MySQL
+
 hname=$(mysql -uroot -praspberry -se "SELECT hostname FROM atomik_controller.atomik_settings");
 echo $hname > /etc/hostname;
 
