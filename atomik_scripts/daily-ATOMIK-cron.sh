2e7003e5e0363b57dc4524245e2314440322ff5f
Rahim Khoja <rahimk@khojacorp.com>
2016-07-11 20:48:07 +0000

    daily-ATOMIK-cron.sh edited online with Bitbucket

--- atomik_scripts/daily-ATOMIK-cron.sh
+++ atomik_scripts/daily-ATOMIK-cron.sh
@@ -4,6 +4,8 @@
 # Bash
 # By Rahim Khoja
 
+# Requires Root Crontab
+
 # Delete Received Commands Older Than 1 Day
 TIMEZONE="$(cat /etc/timezone | awk '{print $1}')"
 mysql -D atomik_controller -u root -praspberry -e "DELETE FROM atomik_commands_received WHERE command_received_date <= CONVERT_TZ(NOW(), '$TIMEZONE', 'UTC') - INTERVAL 24 HOUR;"
