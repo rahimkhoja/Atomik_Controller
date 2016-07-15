7e4a6b1e7eb4d17fd43abae9e2315ad7777790c8
Rahim Khoja <rahimk@khojacorp.com>
2016-07-15 21:01:15 +0000

    transceiver.cpp edited online with Bitbucket

--- atomik_transceiver/transceiver.cpp
+++ atomik_transceiver/transceiver.cpp
@@ -1,4 +1,4 @@
-//  sudo g++ -std=c++11 -lrf24-bcm -lcurl -pthread -L/usr/lib -lmysqlcppconn -I/usr/include/cppconn atomik_transceiver/PL1167_nRF24.cpp atomik_transceiver/MiLightRadio.cpp atomik_transceiver/transceiver.cpp atomik_cypher/atomikCypher.cpp -o transceiver
+// sudo g++ -std=c++11 -lrf24-bcm -lcurl -pthread -ljsoncpp -L/usr/lib -lmysqlcppconn -I/usr/include/cppconn atomik_transceiver/PL1167_nRF24.cpp atomik_transceiver/MiLightRadio.cpp atomik_transceiver/transceiver.cpp atomik_cypher/atomikCypher.cpp -o transceiver
 // Atomik Transciever
 // C / C++
 // By Rahim Khoja
